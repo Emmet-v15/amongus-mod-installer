5b60db250e2ad088f4e66d801b863e4f4862c56f
Emmet_v15 <emmet@v15.studio>
2024-08-23 03:27:49 +0100

    Added safety checks and updated script output

--- installer.ps1
+++ installer.ps1
@@ -1,28 +1,28 @@
-Write-Host "Downloading Among Us..."
 $url = "https://github.com/TheOtherRolesAU/TheOtherRoles/releases/download/v4.6.0/TheOtherRoles.zip"
 $output = "$env:TEMP\TheOtherRoles.zip"
 if (Test-Path -Path $output) {
     $checksum = Get-FileHash -Path $output -Algorithm SHA256
     if ($checksum.Hash -eq "7300fc6125bd0b36a7d9d92befaaf98e6b2b24ebd587714c2bef1fa1cdddc813") {
-        Write-Host "The other roles already downloaded"
+        Write-Host "The Other Roles mod file already downloaded." -ForegroundColor Red 
     } else {
-        Write-Host "The other roles checksum mismatch, redownloading..."
+        Write-Host "The Other Roles mod file checksum mismatch, redownloading..." -ForegroundColor Red
         Remove-Item -Path $output -Force
     }
 } else {
+    Write-Host "Downloading The Other Roles..." -ForegroundColor Red
     Invoke-WebRequest -Uri $url -OutFile $output
 }
 
 $APPID = 945360
 $steamPath = Get-ItemProperty -Path "HKCU:\Software\Valve\Steam" -Name "SteamPath"
-$steamLibraryFolders = Get-Content -Path "$($steamPath.SteamPath)/steamapps/libraryfolders.vdf"
+$steamLibraryFolders = Get-Content -Path "$($steamPath.SteamPath)\steamapps\libraryfolders.vdf"
 $steamLibraryFolders = $steamLibraryFolders | Select-String -Pattern "path"
 $pattern = '^\s+"path"\s+"(.+?)"'
 foreach ($line in $steamLibraryFolders) {
     if ($line -match $pattern) {
         $path = $matches[1]
         $normalizedPath = $path -replace '\\\\', '\'
-        $normalizedPath = $normalizedPath + "/steamapps/"
+        $normalizedPath = $normalizedPath + "\steamapps"
         $files = Get-ChildItem -Path $normalizedPath -Filter "*$APPID.acf"
         $foundAmongUsManifest = 0
         if ($files.Count -eq 1) {
@@ -30,25 +30,30 @@ foreach ($line in $steamLibraryFolders) {
             $foundAmongUsManifest = 1
             $normalPath = "$normalizedPath\common\Among Us"
             $publicPrevious = $fileContent | Select-String -Pattern "public-previous"
-            Write-Output $publicPrevious
             if ($null -ne $publicPrevious) {
-                Write-Host "BetaKey already added in $file, is the script already running?"
+                Write-Warning "BetaKey already added in $file, is the script already running?"
                 Pause
                 return
             }
             $fileContent = Get-Content -Path $file.FullName
             $betaKeyLine = $fileContent | Select-String -Pattern "BetaKey"
 
-            Write-Host "Closing Steam..."
+            Write-Host "Closing Steam..." -ForegroundColor Red
             Get-Process -Name "steam*" -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
+            $process = Get-Process -Name "steam*" -ErrorAction SilentlyContinue
+            while ($null -ne $process) {
+                Start-Sleep -Seconds 1
+                $process = Get-Process -Name "steam*" -ErrorAction SilentlyContinue
+            }
+            Write-Host "Steam closed" -ForegroundColor Red
 
             if (Test-Path -Path $normalPath) {
                 $backupPath = "$normalizedPath\common\Among Us Backup"
                 if (Test-Path -Path $backupPath) {
                     Remove-Item -Path $backupPath -Recurse -Force
                 }
+                Write-Host "Backing up Among Us to ""$backupPath""" -ForegroundColor Red
                 Copy-Item -Path $normalPath -Destination $backupPath -Recurse
-                Write-Host "Among Us folder backed up to $backupPath"
             }
 
             $fileContent = Get-Content -Path $file.FullName
@@ -60,21 +65,22 @@ foreach ($line in $steamLibraryFolders) {
                 $fileContent[$lineNumber] = $fileContent[$lineNumber] -replace "{", "{`n""BetaKey"" ""public-previous"""
                 Set-Content -Path $file.FullName -Value $fileContent
                 $file
-                Write-Host "BetaKey added in $file"
+                Write-Host "BetaKey added for public-previous in ""$file""" -ForegroundColor Red
             } else {
                 $fileContent = $fileContent -replace """public""", """public-previous"""
                 Set-Content -Path $file.FullName -Value $fileContent
-                Write-Host "BetaKey edited in $file"
+                Write-Host "BetaKey edited to public-previous in ""$file""" -ForegroundColor Red
             }
         }
     }
 }
 if ($foundAmongUsManifest -eq 0) {
-    Write-Host "Among Us not found in any Steam library folder"
+    Write-Warning "Among Us not found in any Steam library folder."
 } else {
-    Write-Host "Among Us found in $normalizedPath"
-    Write-Host "Waiting for Among Us Public Previous (6.18) to download..."
-    Start-Process -FilePath "$($steamPath.SteamPath)/steam.exe" -ArgumentList "-applaunch $APPID"
+    Write-Host "Among Us found in ""$normalizedPath""" -ForegroundColor Red
+
+    Write-Host "Waiting for Among Us Public Previous (v2024.6.18) to download..." -ForegroundColor Red
+    Start-Process -FilePath "$($steamPath.SteamPath)\steam.exe" -ArgumentList "-applaunch $APPID"
     $process = Get-Process -Name "Among Us" -ErrorAction SilentlyContinue
     while ($null -eq $process) {
         Start-Sleep -Seconds 1
@@ -82,13 +88,12 @@ if ($foundAmongUsManifest -eq 0) {
     }
     Get-Process -Name "steam*" -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
     Get-Process -Name "among us*" -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
-    Start-Sleep -Seconds 1
-
-    Write-Host "Among Us Public Previous downloaded (6.18)"
+    Start-Sleep -Seconds 3
+    Write-Host "Among Us (v2024.6.18) installed" -ForegroundColor Red
     
     $fileContent = $fileContent -replace """public-previous""", """public"""
     Set-Content -Path $file.FullName -Value $fileContent
-    Write-Host "BetaKey edited in $file"
+    Write-Host "BetaKey revereted to public in ""$file""" -ForegroundColor Red
 
     if (Test-Path -Path $normalPath) {
         $moddedPath = "$normalizedPath\common\Among Us Modded"
@@ -96,17 +101,17 @@ if ($foundAmongUsManifest -eq 0) {
             Remove-Item -Path $moddedPath -Recurse -Force
         }
         Move-Item -Path $normalPath -Destination $moddedPath
-        Write-Host "Among Us modded folder moved to $moddedPath"
+        Write-Host "Among Us modded folder moved to ""$moddedPath""" -ForegroundColor Red
         if (Test-Path -Path $normalPath) {
             Remove-Item -Path $normalPath -Recurse -Force
         }
         Move-Item -Path $backupPath -Destination $normalPath
-        Write-Host "Among Us backup folder moved to $normalPath"
+        Write-Host "Among Us backup folder moved to ""$normalPath""" -ForegroundColor Red
     }
 
     Expand-Archive -Path $output -Destination $moddedPath -Force
-    Write-Host "The other roles extracted to $moddedPath"
-    Start-Process -FilePath "$($steamPath.SteamPath)/steam.exe"
+    Write-Host "The other roles extracted to ""$moddedPath""" -ForegroundColor Red
+    Start-Process -FilePath "$($steamPath.SteamPath)\steam.exe" 
 
     $targetPath = $moddedPath + "\Among Us.exe"
     $shortcutPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('StartMenu'), 'Programs', 'Among Us Modded.lnk')
@@ -122,7 +127,7 @@ if ($foundAmongUsManifest -eq 0) {
     $shortcut.WorkingDirectory = [System.IO.Path]::GetDirectoryName($targetPath)
     $shortcut.IconLocation = $targetPath
     $shortcut.Save()
-
-    Write-Host "Modded Among Us is installed! Have fun!"
+    Write-Host "Shortcut created in Start Menu and Desktop" -ForegroundColor Red
+    Write-Host "Among Us Modded is ready to play, Have fun!" -ForegroundColor 
     Pause
 }
